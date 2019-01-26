1312515e01d54544b535d1b978f27d1ccd2e69dc
furuya1223 <kouki23.bass@gmail.com>
2019-01-26 15:24:43 +0900

    modify host url

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ def message_received(client, server, message):
 
 # Main
 def main():
-    server = WebsocketServer(port=3128, host='pywebsocket-test.com', loglevel=logging.INFO)
+    server = WebsocketServer(port=3128, host='pywebsocket-test.herokuapp.com', loglevel=logging.INFO)
     server.set_fn_new_client(new_client)
     server.set_fn_client_left(client_left)
     server.set_fn_message_received(message_received)
