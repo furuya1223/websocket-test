6adffaac4f4aab2c46e22a0471491912131ecc27
furuya1223 <kouki23.bass@gmail.com>
2019-01-26 15:34:30 +0900

    add debug message

--- server.py
+++ server.py
@@ -23,6 +23,7 @@ def message_received(client, server, message):
 
 # Main
 def main():
+    print('start')
     server = WebsocketServer(port=3128, host='pywebsocket-test.herokuapp.com', loglevel=logging.INFO)
     server.set_fn_new_client(new_client)
     server.set_fn_client_left(client_left)
